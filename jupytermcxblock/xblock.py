269e99fcb7e8a350a7e9f057d72ba773bcb96fa2
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-11-25 09:41:02 -0500

    overwrite ltixblock method to always send username as this is needed for MC

--- jupytermcxblock/xblock.py
+++ jupytermcxblock/xblock.py
@@ -130,6 +130,100 @@ class JupyterMCXBlock(LtiConsumerXBlock):
 
         return custom_parameters
 
+    @XBlock.handler
+    def lti_launch_handler(self, request, suffix=''):  # pylint: disable=unused-argument
+        """
+        XBlock handler for launching LTI 1.1 tools.
+
+        Displays a form which is submitted via Javascript
+        to send the LTI launch POST request to the LTI
+        provider.
+
+        Arguments:
+            request (xblock.django.request.DjangoWebobRequest): Request object for current HTTP request
+            suffix (unicode): Request path after "lti_launch_handler/"
+
+        Returns:
+            webob.response: HTML LTI launch form
+        """
+        lti_consumer = self._get_lti_consumer()
+
+        # Occassionally, users try to do an LTI launch while they are unauthenticated. It is not known why this occurs.
+        # Sometimes, it is due to a web crawlers; other times, it is due to actual users of the platform. Regardless,
+        # return a 400 response with an appropriate error template.
+        try:
+            real_user_data = self.extract_real_user_data()
+            user_id = self.get_lti_1p1_user_id()
+            role = self.role
+
+            # Convert the LMS role into an LTI 1.1 role.
+            role = LTI_1P1_ROLE_MAP.get(role, 'Student,Learner')
+
+            result_sourcedid = self.lis_result_sourcedid
+        # Fails if extract_real_user_data() fails
+        except LtiError as err:
+            loader = ResourceLoader(__name__)
+            template = loader.render_django_template('/templates/html/lti_launch_error.html',
+                                                     context={"error_msg": err})
+            return Response(template, status=400, content_type='text/html')
+
+        username = None
+        full_name = None
+        email = None
+
+        # Always send username as this is needed
+        if real_user_data['user_username']:
+            username = real_user_data['user_username']
+
+        lti_consumer.set_user_data(
+            user_id,
+            role,
+            result_sourcedid=result_sourcedid,
+            person_sourcedid=username,
+            person_contact_email_primary=email,
+            person_name_full=full_name,
+        )
+
+        lti_consumer.set_context_data(
+            self.context_id,
+            self.course.display_name_with_default,
+            self.course.display_org_with_default
+        )
+
+        if self.has_score:
+            lti_consumer.set_outcome_service_url(self.outcome_service_url)
+
+        if real_user_data['user_language']:
+            lti_consumer.set_launch_presentation_locale(real_user_data['user_language'])
+
+        lti_consumer.set_custom_parameters(self.prefixed_custom_parameters)
+
+        for processor in self.get_parameter_processors():
+            try:
+                default_params = getattr(processor, 'lti_xblock_default_params', {})
+                lti_consumer.set_extra_claims(default_params)
+                lti_consumer.set_extra_claims(processor(self) or {})
+            except Exception:  # pylint: disable=broad-except
+                # Log the error without causing a 500-error.
+                # Useful for catching casual runtime errors in the processors.
+                log.exception('Error in XBlock LTI parameter processor "%s"', processor)
+
+        lti_parameters = lti_consumer.generate_launch_request(self.resource_link_id)
+
+        # emit tracking event
+        event = {
+            'lti_version': lti_parameters.get('lti_version'),
+            'user_roles': lti_parameters.get('roles'),
+            'launch_url': lti_consumer.lti_launch_url,
+        }
+        track_event('xblock.launch_request', event)
+
+        loader = ResourceLoader(__name__)
+        context = self._get_context_for_template()
+        context.update({'lti_parameters': lti_parameters})
+        template = loader.render_django_template('/templates/html/lti_launch.html', context)
+        return Response(template, content_type='text/html')
+
     # Fix student view
     @XBlock.supports("multi_device")
     def student_view(self, context):
